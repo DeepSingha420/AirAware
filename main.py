ee1ec6c4c1e942f4a45b3a94496fbafec49bed80
DeathWish <76990917+DeepSingha420@users.noreply.github.com>
2026-02-08 20:43:21 +0530

    AirAware

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
-from routers import aqi  # 1. Import the router file
+from routers import aqi
 from loguru import logger
 
 app = FastAPI()
 
-# 2. Tell the app to include the router's logic
 app.include_router(aqi.router)
 
 @app.get("/health")
 async def health_check():
     logger.info("Health check endpoint called")
+
     return {"status": "ok"}
